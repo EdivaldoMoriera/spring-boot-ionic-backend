ed35c0f2a3baba416ce46ecaa4d7edc909127d1f
Moreira <valdosm.moreira@gmail.com>
2023-01-10 06:17:47 -0300

    Paginação

--- src/main/java/com/valdosm/cursomc/CursomcApplication.java
+++ src/main/java/com/valdosm/cursomc/CursomcApplication.java
@@ -60,6 +60,12 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Cama, Mesa e banho");
+		Categoria cat4 = new Categoria(null, "Jardinagem");
+		Categoria cat5 = new Categoria(null, "Perfumaria");
+		Categoria cat6 = new Categoria(null, "Ferramentas");
+		Categoria cat7 = new Categoria(null, "Eletronicos");
+		Categoria cat8 = new Categoria(null, "Decoração");
 
 		Produto p1 = new Produto(null,"Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -88,7 +94,7 @@ public class CursomcApplication implements CommandLineRunner {
 		est3.getCidades().addAll(Arrays.asList(cid4));
 
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2, est3));
